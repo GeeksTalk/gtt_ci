99d0a51112bccf73f375d80ba3376850ca198e8c
Brian Gikonyo <colasgikonyo@gmail.com>
2020-02-27 15:57:16 +0300

    gtt travis ci demo

--- gtt_ci/new_app/tests.py
+++ gtt_ci/new_app/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 class GttCi(TestCase):
     resp_code = 200
-    random_chars = [" ", " ", " ", " not ", " ", " ", " ", " not ", " ", " ", " ", " not ", " ", " ", " ", " not "]
+    random_chars = [" ", " not", " ", " not ", " ", " ", " ", " not ", " ", " ", " ", " not ", " ", " ", " ", " not "]
     resp_text1 = "This is not an acceptable response."
     resp_text2 = "This is{}an acceptable response.".format(random.choice(random_chars))
 
